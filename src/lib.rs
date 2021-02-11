16124664e0f4105f85886c353025cb028f0d1824
Durid Ahmad <ahmad.durid96@gmail.com>
2021-02-10 18:08:05 -0700

    Documented the worker struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 use std::thread;
 use std::sync::{mpsc, Arc, Mutex};
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+/// The worker contains a thread that will wait on the sender
+/// to provide a job to execute
 pub struct Worker {
    id: usize,
    thread: thread::JoinHandle<()>
 }
 
 impl Worker {
+   /// Create a new worker with an id, and a receiver to listen for new jobs
    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
       let thread = thread::spawn(move || loop {
          let job = receiver.lock().unwrap().recv().unwrap();
